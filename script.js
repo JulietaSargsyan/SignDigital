4d1b4da1809f51915569240c3fdba4fc048f9e19
Julieta Sargsyan <julietasargsian@gmail.com>
2022-08-05 23:20:21 +0400

    added responsivness

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ window.onscroll = function(ev) {
     // let css_of_before = window.getComputedStyle(line, '::before');
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
         line.style.setProperty('--beforeDisplay', 'block');
-        // line.style.cssText += 'cursor:pointer';  
         lineA.style.cursor = 'pointer';
+        line.style.cursor = 'pointer';
         line.classList.add('lineAnimation');
+        line.addEventListener('mouseover', () => line.classList.add('lineAnimationHover') )
+        line.addEventListener('mouseout', () => line.classList.remove('lineAnimationHover') )
+
     } else {
         line.style.setProperty('--beforeDisplay', 'none')
         lineA.style.cursor = 'default';
         line.classList.remove('lineAnimation');
+        lineA.addEventListener('mouseover', () => lineA.classList.remove('lineAnimationHover') )
     }
 };
 
